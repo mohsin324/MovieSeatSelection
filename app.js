4ac62c4518c307c4fc39e857fb302d20ec6d7b1e
mohsin324 <mohsinmunir324@gmail.com>
2022-09-24 21:31:23 +0500

    completed movie seat selection project

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 //getting container
 const container = document.querySelector('.container');
-console.log(container);
+//getting movies
+const movie = document.getElementById('movie');
+// getting movie value for creating ticket price
+let ticketPrice = movie.value;
+// console.log(ticketPrice);
+//total no of seats
+const totalSeat = document.querySelectorAll('.row .column.booked');
+//getting total no of booked seats and amount
+const noOfBookedSeat = document.getElementById('bookSeat');
+noOfBookedSeat.innerHTML = totalSeat.length;
+//getting amount
+const amount = document.getElementById('amount');
+amount.innerHTML = ticketPrice
+
+//function for updating total no of seats and price
+function updateSeat(){
+    const totalSeat = document.querySelectorAll('.row .column.booked');
+    // console.log(totalSeat.length);
+    noOfBookedSeat.innerHTML = totalSeat.length;
+    // console.log('ticket price'+ ticketPrice);
+    amount.innerHTML = ticketPrice * noOfBookedSeat.innerHTML
+
+}
+//add event listener for calculating ticket price
+movie.addEventListener('change', e => {
+    ticketPrice = +e.target.value
+    // console.log('ticket price'+ ticketPrice);
+    amount.innerHTML = ticketPrice * noOfBookedSeat.innerHTML;
+})
+
+// add event listener for booking and cancelling seats
 container.addEventListener('click', e => {
     if(e.target.classList.contains('column') && !e.target.classList.contains('column booked')){
-        e.target.classList.toggle('booked')
+        e.target.classList.toggle('booked');
+        updateSeat()
     }
-})
+});
